323f5566409fb0ed224ecc4f4422a2b7c0ca1289
Philippa Louise Giibwa <125802100+PhiLouGii@users.noreply.github.com>
2023-12-12 22:04:16 +0200

    deleted file

--- main.js
+++ main.js
@@ -1,19 +1,1 @@
-javascript
-const apiKey = 'YOUR_API_KEY';
-const city = 'New York';
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    const temperature = data.main.temp;
-    const description = data.weather[0].description;
-    const icon = data.weather[0].icon;
-    const iconUrl = `http://openweathermap.org/img/w/${icon}.png`;
-
-    // Display the weather data in the UI
-    document.getElementById('temperature').textContent = temperature;
-    document.getElementById('description').textContent = description;
-    document.getElementById('icon').setAttribute('src', iconUrl);
-  })
-  .catch(error => console.error(error));
